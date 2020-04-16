a91fe1cd4c5c0539a8841057a7b1f40adc242777
Mistry, Aneesh <aneesh.mistry@accenture.com>
2020-04-16 20:53:45 +0100

    solved organising container of balls

---
+++ src/com/aneesh/problemSolvingAndAlgorithms/OrganisingContainerOfBalls.java
@@ -0,0 +1,54 @@
+package com.aneesh.problemSolvingAndAlgorithms;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class OrganisingContainerOfBalls {
+
+
+    static String organizingContainers(int[][] container) {
+
+        //Time Complexity = O(n + m)
+
+        List<Integer> tableSizes = new ArrayList<>();
+        List<Integer> ballNumbers = new LinkedList<>();
+
+        for (int a = 0; a < container.length; a++){
+            ballNumbers.add(0);
+        }
+
+     for (int j = 0; j< container.length; j++) {
+
+         int[] Queries = container[j];
+         int sum = 0;
+         for (int i = 0; i< container.length; i++) {
+             ballNumbers.set(i,ballNumbers.get(i) + Queries[i]);
+             sum = sum + Queries[i];
+         }
+         tableSizes.add(sum);
+     }
+
+        tableSizes.sort((a,b) -> a.compareTo(b));
+        ballNumbers.sort(Comparator.naturalOrder());
+        String result = "Impossible";
+
+        if(tableSizes.equals(ballNumbers)){
+            result = "Possible";
+        }
+
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+        int[][] a = {{3,1,0},{0,2,1}, {1,0,3}};
+        System.out.println(organizingContainers(a));
+
+    }
+}
