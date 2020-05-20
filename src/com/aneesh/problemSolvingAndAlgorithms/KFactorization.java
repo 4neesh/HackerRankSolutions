958afe2fd5ddf81b435381e17a29f07690dec2b5
Mistry, Aneesh <aneesh.mistry@accenture.com>
2020-05-20 19:44:03 +0100

    greedy algo

--- src/com/aneesh/problemSolvingAndAlgorithms/KFactorization.java
+++ src/com/aneesh/problemSolvingAndAlgorithms/KFactorization.java
@@ -1,13 +1,52 @@
 package com.aneesh.problemSolvingAndAlgorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KFactorization {
 
     //link to problem: https://www.hackerrank.com/challenges/k-factorization/problem
 
-    
-    public static void main(String[] args) {
 
+    static int[] kFactorization(int target, int[] values){
+
+        int[] result = {0};
+
+
+        //eliminate values that don't go into target
+        List<Integer> factorsToTarget = new ArrayList<>();
+        for(int i : values){
+            if( target % i == 0){
+                factorsToTarget.add(i);
+            }
+        }
+
+        System.out.println(factorsToTarget);
+        //find all combinations
+        //use greedy algorithm to find the value
+
+
+
+        //find lexicographical one
+
+
+
+
+        //array has the same number as the size of lexicographical size
+
+
+        //return it
+        return result;
 
 
     }
+    public static void main(String[] args) {
+
+        int[] values = {2,4,6,9,3,7,16,10,5};
+        kFactorization(72, values);
+        
+
+    }
+
+
 }
